35720643d453f0f826a663f7bbbb185c2493e2f9
blackkeyboard141 <rasik.hasan@yahoo.com>
2018-07-15 11:27:00 +0600

    Initial commit

---
+++ app/src/main/java/com/devai/rasik/e_learn/MainActivity.java
@@ -0,0 +1,17 @@
+package com.devai.rasik.e_learn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);}
+
+
+
+}
